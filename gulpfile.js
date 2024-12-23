af4828383e930bb87a40f69ffdc5e8c1ba5f2389
Luke <lukestrazzera@gmail.com>
2024-12-22 21:37:16 -0500

    new site, woo

--- gulpfile.js
+++ gulpfile.js
@@ -20,9 +20,21 @@ gulp.task('copy-html', function() {
         .pipe(gulp.dest('docs'));
 });
 
+gulp.task('copy-main', function() {
+    return gulp.src('main.html')
+        .pipe(gulp.dest('docs'));
+});
+
 // Copy CSS
 gulp.task('copy-css', function() {
     return gulp.src('styles.css')
+        .pipe(postcss([cssnano()]))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('docs'));
+});
+
+gulp.task('copy-maincss', function() {
+    return gulp.src('main.css')
         .pipe(postcss([cssnano()]))
         .pipe(rename({suffix: '.min'}))
         .pipe(gulp.dest('docs'));
@@ -48,9 +60,10 @@ gulp.task('connect', function() {
 // Watch files for changes
 gulp.task('watch', function() {
     gulp.watch(paths.html, gulp.series('copy-html'));
+    gulp.watch(paths.html, gulp.series('copy-main'));
     gulp.watch(paths.css, gulp.series('copy-css'));
     gulp.watch(paths.assets, gulp.series('copy-assets'));
 });
 
 // Default Task
-gulp.task('default', gulp.series('copy-html', 'copy-css', 'copy-assets', 'connect', 'watch'));
+gulp.task('default', gulp.series('copy-html', 'copy-main', 'copy-css', 'copy-maincss', 'copy-assets', 'connect', 'watch'));
